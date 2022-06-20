8f7dfb39efe02262630616be43b129a262a6895d
Akoh Samuel <akoh.ojimaojo@umba.com>
2022-06-20 21:59:32 +0100

    second commit

--- src/components/TaskModal.js
+++ src/components/TaskModal.js
@@ -4,27 +4,8 @@ import { Modal, Button } from "react-bootstrap";
 function TaskModal(props) {
    
     return (
-      
-        // <div className={showHideClassName} id="exampleModal" tabIndex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-        //     <div className="modal-dialog" role="document">
-        //     <div className="modal-content">
-        //         <div className="modal-header">
-        //         <h5 className="modal-title" id="exampleModalLabel">Modal title</h5>
-        //         <button type="button" className="close" data-dismiss="modal" aria-label="Close">
-        //             <span aria-hidden="true">&times;</span>
-        //         </button>
-        //         </div>
-        //         <div className="modal-body">
-        //         ...
-        //         </div>
-        //         <div className="modal-footer">
-        //         <button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
-        //         <button type="button" className="btn btn-primary">Save changes</button>
-        //         </div>
-        //     </div>
-        //     </div>
-        // </div>
-        <Modal show={props.isHidden} onHide={props.onClose}
+    
+        <Modal.Dialog show={props.isHidden} onHide={props.onClose}
         parentselector={() => document.querySelector('#root')}>
             <Modal.Header closeButton>
                 <Modal.Title>Translations</Modal.Title>
@@ -38,7 +19,7 @@ function TaskModal(props) {
             <Button onClick={props.onClose}>Save</Button>
                 <Button onClick={props.onClose}>Close</Button>
             </Modal.Footer>
-            </Modal>
+            </Modal.Dialog>
       
     )
 }
--- src/components/TaskTable.js
+++ src/components/TaskTable.js
@@ -43,7 +43,25 @@ function TaskTable() {
        
       <div className="task-table">
         
-      <TaskModal isHidden={isOpen} onClose={hideModal}/>
+      <div className="modal fade" id="exampleModal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div className="modal-dialog">
+            <div className="modal-content">
+            <div className="modal-header">
+                <h5 className="modal-title" id="exampleModalLabel">Modal title</h5>
+                <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div className="modal-body">
+                ...
+            </div>
+            <div className="modal-footer">
+                <button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
+                <button type="button" className="btn btn-primary">Save changes</button>
+            </div>
+            </div>
+        </div>
+        </div>
     
         
         <div className="form-task">
@@ -69,7 +87,7 @@ function TaskTable() {
                     <th scope="col">
                         {/* <button className="btn" data-toggle="modal" data-target="#exampleModal">
                             <i className="bi bi-plus-circle"></i></button> */}
-                        <button className="btn" onClick={showModal}>
+                        <button className="btn" data-toggle="modal" data-target="#exampleModal">
                             <i className="bi bi-plus-circle"></i></button>
                     </th>
                 {
